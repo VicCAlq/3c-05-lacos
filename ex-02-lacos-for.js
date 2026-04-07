42e864ced28225e2ead7ba728ec4a2a67ab8782a
Manuella Siqueira <manuella.siqueira@nave.org.br>
2026-04-07 00:06:17 -0300

    Exercício 2 de For completo

--- ex-02-lacos-for.js
+++ ex-02-lacos-for.js
@@ -1,38 +1,48 @@
 /*
 Assunto 02 - Laços de repetição: For
 
+
 A outra forma principal de executar um laço de repetição é utilizando o "for".
 Ele segue os mesmos princípios, mas segue uma sintaxe diferente:
+
 
 // PSEUDO-CÓDIGO
 for (valor inicial do contador; condição para o contador; mudança do contador) {
   código a ser executado repetidamente
 }
 
+
 // CÓDIGO REAL
 for (let contador = 0; contador <= 20; contador = contador + 1) {
   console.log(contador)
 }
 
+
 Vamos comparar com o loop de while do exercício anterior:
 
+
 let contador = 0
+
 
 while (contador <= 20) {
   console.log(contador)
   contador = contador + 1
 }
 
-A diferença principal é que na sintaxe do for incluimos tudo referente ao 
+
+A diferença principal é que na sintaxe do for incluimos tudo referente ao
 contador na mesma linha: A criação e valor inicial dele, a condição de término,
 e como este contador é modificado. O que isso muda para nós?
 
+
 1. No caso do "for", o contador não fica disponível para uso fora do "for".
-   Na linha que o "for" acaba o contador deixa de existir. 
+   Na linha que o "for" acaba o contador deixa de existir.
 2. No caso do "while", como o contador é criado externamente, ele segue existindo
    mesmo após o término do laço.
 
+
 E para quais situações cada um é mais adequado?
+
 
 - É mais recomendado usar "for" quando queremos manipular uma quantidade CONHECIDA
   de itens, ou quando queremos executar as instruções uma quantidade definida
@@ -42,47 +52,65 @@ E para quais situações cada um é mais adequado?
   de "inputs" (exemplo: Enquanto eu pressionar a seta para a direita, meu personagem
   se move para a direita. No código não temos como representar isso com um "for").
 
+
 Vamos ver os exemplos do exercício anterior na sintaxe de for:
 
+
 1. Realizar algo em sequência:
+
 
 for (let comeco = 0; comeco <= fim; comeco++) {
   console.log(comeco)
 }
 
+
 - O código acima imprime os números de 0 a 10
 
+
 2. Realizar algo em sequência inversa:
+
 
 for (let comeco = 10; comeco >= fim; comeco--) {
   console.log(comeco)
 }
 
+
 - O código acima imprime os números de 10 a 0
+
 
 3. Acumular valores:
 
+
 let acumulador = 0
+
 
 for (let valor = 3; valor <= 9; valor = valor + 1) {
   acumulador += valor
 }
 
+
 - O código acima vai calcular a soma de todos os números de 3 a 9 (é 42)
+
 
 4. Manipular itens de uma lista:
 
+
 let nomes = ["arnoldo", "betolino", "carmem", "denise", "érica", "fátima"]
+
 
 for (let indice = 0; indice < nomes.length; indice += 1) {
   nomes[indice] = nomes[indice].toUpperCase()
 }
 
+
 - O código acima acessa todos os itens da lista e deixa em maiúsculas
+
 
 5. Manipular itens de uma lista de acordo com alguma condição:
 
+
 let numeros = [3, 4, 6, 7, 9, 11, 12, 13, 14, 16, 18, 19]
+
 
 for (let indice = 0; indice < numeros.length; indice += 1) {
   if (numeros[indice] % 2 === 0) {
@@ -90,25 +118,33 @@ for (let indice = 0; indice < numeros.length; indice += 1) {
   }
 }
 
+
 - O código acima só imprime os números pares da lista
+
 
 6. Realizar uma operação pulando alguns itens:
 
+
 let numeros = [3, 4, 6, 7, 9, 11, 12, 13, 14, 16, 18, 19]
+
 
 for (let indice = 0; indice < numeros.length; indice += 3) {
   console.log(numeros[indice])
 }
 
+
 - O código acima imprime os números da lista pulando de 3 em 3 índices
+
 
 7. Extrair valores de uma lista e criar outra
 
+
 const nomes = [
-  "arnoldo", "betolino", "cadu", "denise", "érica", 
+  "arnoldo", "betolino", "cadu", "denise", "érica",
   "ferdinanda", "gina", "helen", "ingride", "jeremias"
 ]
 const nomesLongos = []
+
 
 for (let indice = 0; indice < nomes.length; indice += 1) {
   if (nomes[indice].length > 6) {
@@ -116,35 +152,44 @@ for (let indice = 0; indice < nomes.length; indice += 1) {
   }
 }
 
+
 - O código acima vai copiar todos os nomes com mais de 6 letras para a lista
 de nomes longos
+
 
 Da mesma forma que ocorre no "while", é necessário cuidado com a condição
 e incremento do contador, pois mesmo em um "for" é possível criar situações
 indesejadas:
 
+
 for (let contador = 0; contador !== 5; contador += 2) {
   // resulta em um loop infinito: Mesmo caso do exemplo do exercício 01
 }
 
+
 Como frequentemente utilizamos o loop de for para interagir com listas, ele possui
 também uma outra sintaxe, mais diretamente voltada para isso:
+
 
 // EM PSEUDO-CÓDIGO:
 for (cada item da lista) {
   código a ser executado
 }
 
+
 // EM CÓDIGO REAL:
 const lista = ["casa", "camelo", "cera", "coxinha", "cinto", "cocada", "computador", "cd"]
+
 
 for (let item of lista) {
   console.log(item)
 }
 
+
 Nesta versão do loop, não precisamos acessar cada item da lista usando a sintaxe
 "nomeDaLista[indice]". A variável criada dentro do "for" (chamada de "item" neste
 exemplo) é automaticamente atualizada com o valor dos itens da lista em sequência:
+
 
 1. Na primeira vez que o código dentro do "for" é executado, o valor de "item" é "casa"
 2. Na segunda vez, o valor de "item" é "camelo"
@@ -152,37 +197,48 @@ exemplo) é automaticamente atualizada com o valor dos itens da lista em sequên
 4. Na quarta vez, o valor de "item" é "coxinha"
 5. E por aí vai.
 
+
 Não é necessário explicitar onde fica o fim da lista, pois neste caso o "for"
 já sabe onde fica.
 
+
 E qual a desvantagem disso?
 
+
 1. Não é possível utilizar os índices das listas (sem utilizar um método de lista
-   específico pra isso). Isso impede percorrer e utilizar duas ou mais listas de 
-   mesmo tamanho ao mesmo tempo. O exemplo abaixo não é possível na sintaxe 
+   específico pra isso). Isso impede percorrer e utilizar duas ou mais listas de
+   mesmo tamanho ao mesmo tempo. O exemplo abaixo não é possível na sintaxe
    "for item of lista":
+
 
    const comida = ["arroz", "macarrão", "queijo", "batata-doce"]
    const acompanhamento = ["feijão", "molho bolonhesa", "goiabada", "frango"]
+
 
    for (let indice = 0; indice < comida.length; indice++) {
      console.log(comida[indice], " com ", acompanhamento[indice])
    }
 
+
    Resulta em:
    "arroz com feijão", "macarrão com molho bolonhesa",
    "queijo com goiabada", "batata-doce com frango"
+
 
 2. Não é possível percorrer a lista em ordem decrescente. Se quiser fazer
    isso é necessário primeiro modificar a lista original invertendo ela.
 3. Não é possível pular itens da lista.
 
+
 ------------------------------------------------------------------------------
+
 
 Questões 01 a 12: Exercícios com laços de repetição do tipo "For"
 
+
 MÍNIMO NECESSÁRIO PARA NOTA MÁXIMA: 8 questões
 */
+
 
 /* Questão 01
 Utilizando um laço de repetição for, calcule a soma de todos os números de 1 a 100
@@ -190,21 +246,26 @@ Utilizando um laço de repetição for, calcule a soma de todos os números de 1
 */
 // Escreva o código da solução abaixo:
 
+
 let final = 100;
 let acumulador = 0;
 let inicio = 1;
+
 
 for( let inicio = 1; inicio <= final; inicio = inicio +1) {
   acumulador += inicio;
 }
 
+
 export const resposta01 = acumulador
+
 
 /* Questão 02
 Utilizando um laço de repetição for, calcule quantos números ímpares existem entre
 1 e 50 (incluindo o 1 e 50). Armazene a quantidade encontrada na variável resposta02.
 */
 // Escreva o código da solução abaixo:
+
 
 /*for (valor inicial do contador; condição para o contador; mudança do contador) {
   código a ser executado repetidamente
@@ -213,25 +274,29 @@ let final = 50;
 let inicio = 1;
 let acumulador = 0;
 
+
 for (let inicio = 1; inicio <= final; inicio += 1) {
   if ( inicio % 2 != 0 ) {
-  console.log(inicio);
+  acumulador++;
 }
 }
+export const resposta02 = acumulador
 
-export const resposta02 = inicio
 
 /* Questão 03
 Utilizando um laço de repetição for, calcule a soma de todos os números pares
-negativos entre -100 e -1 (incluindo o -100). Armazene o resultado na 
+negativos entre -100 e -1 (incluindo o -100). Armazene o resultado na
 variável resposta03.
 */
 // Escreva o código da solução abaixo:
 
 
+
+
 let inicio = -100;
 let final = -1;
 acumulador = 0;
+
 
 for (let inicio = -100; inicio <= final; inicio = inicio + 1) {
    if ( inicio % 2 === 0) {
@@ -239,39 +304,72 @@ for (let inicio = -100; inicio <= final; inicio = inicio + 1) {
    }
 }
 
+
 console.log(acumulador);
 
-export const resposta03 = acumulador 
+
+export const resposta03 = acumulador
+
 
 /* Questão 04
 Utilizando um laço de repetição for, calcule a média aritmética de todos os números
-múltiplos de 3 entre 1 e 200 (incluindo os limites). Armazene o resultado 
+múltiplos de 3 entre 1 e 200 (incluindo os limites). Armazene o resultado
 (pode ser decimal) na variável resposta04.
 */
 // Escreva o código da solução abaixo:
-
 let inicio = 1;
 let final = 200;
+let acumulador = 0;
+let contador = 0;
 
 
-export const resposta04 = false
+for (let inicio = 1; inicio <= final; inicio ++) {
+if (inicio % 3 === 0 ) {
+acumulador += inicio
+  contador++
+}
+}
+
+
+let mediaA = acumulador / contador;
+
+
+export const resposta04 = mediaA
+
 
 // Lista para as questões 5 a 8:
 const itensDomesticos = [
-  "mesa", "cadeira", "sofá", "cortina", "tapete", 
+  "mesa", "cadeira", "sofá", "cortina", "tapete",
   "abajur", "copo", "prato", "panela", "cafeteira",
   "espelho", "quadro", "vaso", "relógio", "almofada"
 ]
+
 
 /* Questão 05
 Dada a lista de itens domésticos acima, utilize um laço de repetição for (com contador)
 para criar um novo array contendo todos os itens que começam com a letra "c".
 Armazene o novo array na variável resposta05.
 
+
 */
 // Escreva o código da solução abaixo:
 
-export const resposta05 = false
+
+const itensComC = [];
+const letraC = "c";
+let indice = 0;
+
+
+for( let indice = 0; indice < itensDomesticos.length; indice += 1) {
+  if(itensDomesticos[indice].startsWith(letraC)) {
+    itensComC.push(itensDomesticos[indice])
+  }
+}
+ 
+
+
+export const resposta05 = itensComC
+
 
 /* Questão 06
 Utilizando a mesma lista de itens domésticos da questão anterior, use um laço de
@@ -281,7 +379,20 @@ Armazene o novo array na variável resposta06.
 */
 // Escreva o código da solução abaixo:
 
-export const resposta06 = false
+
+const itensImpares = [];
+let indice = 0;
+
+
+for(let indice = 0; indice < itensDomesticos.length; indice +=  1) {
+if(indice % 2 !== 0) {
+itensImpares.push(itensDomesticos[indice])
+}
+}
+
+
+export const resposta06 = itensImpares
+
 
 /* Questão 07
 Seguindo com a lista de itens domésticos, use um laço de repetição for (com contador)
@@ -289,23 +400,42 @@ para criar um novo array contendo os itens com mais de 6 letras. Armazene o novo
 na variável resposta07.
 */
 // Escreva o código da solução abaixo:
+const itensLongos = [];
+indice = 0
 
-export const resposta07 = false
+
+for( let indice = 0; indice < itensDomesticos.length; indice += 1) {
+if(itensDomesticos[indice].length > 6) {
+itensLongos.push(itensDomesticos[indice])
+}
+}
+export const resposta07 = itensLongos
+
 
 /* Questão 08
-Finalizando com a lista de itens domésticos abaixo, use um laço de repetição for 
-(com contador) para criar um novo array contendo os itens em ordem reversa. Armazene 
+Finalizando com a lista de itens domésticos abaixo, use um laço de repetição for
+(com contador) para criar um novo array contendo os itens em ordem reversa. Armazene
 o novo array na variável resposta08.
 */
 // Escreva o código da solução abaixo:
 
-export const resposta08 = false
+
+const itensReversa = [];
+indice = itensDomesticos.length - 1
+
+
+for( let indice = itensDomesticos.length - 1; indice >= 0; indice--) {
+itensReversa.push(itensDomesticos[indice]);
+}
+export const resposta08 = itensReversa
+
 
 // Lista para as questões 5 a 10:
 const petsExoticos = [
   "lagarto", "tarântula", "hamster", "cacatua", "iguana",
   "escorpião", "sagui", "jacaré", "serpente", "camaleão"
 ]
+
 
 /* Questão 09
 Dada a lista de nomes de pets exóticos acima, utilize um laço de repetição for...of
@@ -314,13 +444,26 @@ novo array na variável resposta09.
 */
 // Escreva o código da solução abaixo:
 
-export const resposta09 = false
+
+const petsMaiuscula = [];
+let indice = 0;
+
+
+for (let indice = 0; indice < petsExoticos.length; indice += 1) {
+  petsExoticos[indice] = petsExoticos[indice].toUpperCase()
+  petsMaiuscula.push(petsExoticos[indice]);
+}
+
+
+export const resposta09 = petsMaiuscula
+
 
 /* Questão 10
 Com a mesma lista de pets exóticos acima, use um laço de repetição for...of para
 criar um novo array contendo apenas os nomes que possuem a letra "r" em qualquer posição.
 Armazene o novo array na variável resposta10.
 
+
 const petsExoticos = [
   "lagarto", "tarântula", "hamster", "cacatua", "iguana",
   "escorpião", "sagui", "jacaré", "serpente", "camaleão"
@@ -328,13 +471,26 @@ const petsExoticos = [
 */
 // Escreva o código da solução abaixo:
 
-export const resposta10 = false
+
+const petsComR = [];
+const letraR = "r";
+let indice = 0;
+
+
+for( let indice = 0; indice < petsExoticos.length; indice += 1) {
+  if(petsExoticos[indice].includes(letraR)) {
+    petsComR.push(petsExoticos[indice])
+  }
+}
+export const resposta10 = petsComR
+
 
 /* Questão 11
 Continuando com a lista de pets exóticos, use um laço de repetição for...of para
 contar quantos nomes têm mais de 7 letras. Armazene a quantidade encontrada na
 variável resposta11.
 
+
 const petsExoticos = [
   "lagarto", "tarântula", "hamster", "cacatua", "iguana",
   "escorpião", "sagui", "jacaré", "serpente", "camaleão"
@@ -342,13 +498,27 @@ const petsExoticos = [
 */
 // Escreva o código da solução abaixo:
 
-export const resposta11 = false
+
+let pets = 0;
+let indice = 0;
+
+
+for (let indice = 0; indice < petsExoticos.length; indice += 1) {
+  if ( petsExoticos[indice].length > 7 ) {
+  pets++
+}
+}
+
+
+export const resposta11 = pets
+
 
 /* Questão 12
 Encerrando com a lista de pets exóticos, use um laço de repetição for...of para
 encontrar o primeiro nome que começa com a letra "s". Se encontrar, armazene este
 nome na variável resposta12. Se não encontrar nenhum, armazene a string "não encontrado".
 
+
 const petsExoticos = [
   "lagarto", "tarântula", "hamster", "cacatua", "iguana",
   "escorpião", "sagui", "jacaré", "serpente", "camaleão"
@@ -356,4 +526,21 @@ const petsExoticos = [
 */
 // Escreva o código da solução abaixo:
 
-export const resposta12 = false
+
+
+
+let resposta12 = "não encontrado."
+const letraS = "s";
+let indice = 0;
+
+
+for(const petComS of petsExoticos) {
+   if (petComS.startsWith(letraS))  {
+    resposta12 = petComS
+    break
+}
+}
+export const resposta12 = resposta12
+
+
+
